ef8f98adb8c805ef1209214b402c501274634cee
vickydollas <vickybet35@gmail.com>
2025-11-23 22:50:00 +0100

    graph component update

--- src/components/body/DisplayGraph.vue
+++ src/components/body/DisplayGraph.vue
@@ -1,10 +1,23 @@
 <script setup>
+import { ref } from 'vue'
 
+const lineChartSeries = ref([
+    {
+        name: "sales",
+        date: [30, 40, 50, 60, 70]
+    }
+])
+
+const lineChartOptions = ref({
+    xaxis: {
+        categories: ["jan", "feb", "mar", "apr", "may", "june", "july"]
+    }
+})
 </script>
 
 <template>
   <div>
-    <ApexChart 
+    <apexchart 
     type="line"
     :series="lineChartSeries"
     :options="lineChartOptions"
